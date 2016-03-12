8168283126d0044a4cdc2cbf6e2bf03340fb53e7
admin <admin@admins-Mac-Pro-249.local>
2016-03-12 10:07:27 -0700

    made a detail page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 from flask import Flask
+from flask import abort
 from flask import render_template
 app = Flask(__name__)
 
@@ -9,12 +10,23 @@ def get_csv(csv_path):
     csv_list = list(csv_obj)
     return csv_list
 
+
+
 @app.route("/")
 def index():
     template = "index.html"
     object_list = get_csv("./static/la-riots-deaths.csv")
     return render_template(template, object_list=object_list)
 
+
+@app.route("/<row_id>/") 	# here is where code goes 
+def detail(row_id):
+    template = "detail.html"
+    object_list = get_csv("./static/la-riots-deaths.csv")
+    for row in object_list:
+    	if row ['id'] == row_id:
+			return render_template (template, object=row)
+    abort(404)
 
 if __name__ == '__main__':
     # Fire up the Flask test server
